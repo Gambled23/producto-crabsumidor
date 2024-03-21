c4492736ec1a8e47782d2f40925676af6c376a2f
Gambled23 <ipog71@gmail.com>
2024-03-21 12:50:38 -0600

    ARREGLA: el consumidor no consume, y free space no se actualiza

--- src/main.rs
+++ src/main.rs
@@ -8,36 +8,46 @@ fn main() {
     let mut producer_position: u8 = 0;
     let mut consumer_position: u8 = 0;
     let mut random_number: u8;
+    let mut free_space: u8 = 20;
 
     loop {
         random_number = rng.gen_range(1..5);
-        produce(&mut buffer, random_number, &mut producer_position);
+        produce(&mut buffer, random_number, &mut producer_position, &mut free_space);
         println!("Producing {}", random_number);
         println!("Buffer: {:?}", buffer);
 
         thread::sleep(Duration::from_secs(2));
 
         random_number = rng.gen_range(1..5);
-        consume(&mut buffer, random_number, &mut consumer_position);
+        consume(&mut buffer, random_number, &mut consumer_position, &mut free_space);
         println!("Consuming {}", random_number);
         println!("Buffer: {:?}", buffer);
     }
 }
 
-fn produce(buffer: &mut [bool; 20], number: u8, position: &mut u8) {
-    let free_space: u8 = 20 - *position;
+fn produce(buffer: &mut [bool; 20], number: u8, position: &mut u8, free_space: &mut u8) {
 
-    if free_space < number{ // verify enough free space
-        println!("Buffer is full, crabn't produce more.");
+    if *free_space < number{ // verify enough free space
+        println!("Buffer free space is {}, crabn't produce {}.", *free_space, number);
         return;
     }
-
-    for i in 0..number { // fill buffer with the number of elements
-        buffer[(i as usize) + (*position as usize)] = true;
+    
+    for _ in 0..number { // fill buffer with the number of elements
+        buffer[*position as usize] = true;
         *position += 1; 
     }
 }
 
-fn consume(buffer: &mut [bool; 20], number: u8, position: &mut u8) {
+fn consume(buffer: &mut [bool; 20], number: u8, position: &mut u8, free_space: &mut u8) {
+    let product: u8 = 20 - *free_space;
+    if product < number{ // verify enough products to consume
+        println!("Buffer has only {} products, crabn't consume {}.", product, number);
+        return;
+    }
+
+    for i in 0..number { // fill buffer with the number of elements
+        buffer[(i as usize) + (*position as usize)] = false;
+        *position += 1; 
+    }
 
 }
