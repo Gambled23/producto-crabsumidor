bdfe9915f369812e00a465b485412541d146b0ad
Gambled23 <ipog71@gmail.com>
2024-03-21 12:01:58 -0600

    feat: esperar 2 segundos

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::time::Duration;
+use std::thread;
 use rand::Rng;
 
 fn main() {
@@ -5,25 +7,35 @@ fn main() {
     let mut rng = rand::thread_rng();
     let mut producer_position: u8 = 0;
     let mut consumer_position: u8 = 0;
+    let mut random_number: u8;
 
     loop {
-        let random_number: u8 = rng.gen_range(1..5);
-        println!("Random number: {}", random_number);
+        random_number = rng.gen_range(1..5);
         produce(&mut buffer, random_number, &mut producer_position);
+        println!("Producing {}", random_number);
+        println!("Buffer: {:?}", buffer);
+
+        thread::sleep(Duration::from_secs(2));
+
+        random_number = rng.gen_range(1..5);
         consume(&mut buffer, random_number, &mut consumer_position);
+        println!("Consuming {}", random_number);
+        println!("Buffer: {:?}", buffer);
     }
 }
 
 fn produce(buffer: &mut [bool; 20], number: u8, position: &mut u8) {
     let free_space: u8 = 20 - *position;
-    if free_space < number{
+
+    if free_space < number{ // verify enough free space
         println!("Buffer is full, crabn't produce more.");
         return;
     }
-    for i in 0..number {
+
+    for i in 0..number { // fill buffer with the number of elements
         buffer[(i as usize) + (*position as usize)] = true;
+        *position += 1; 
     }
-    *position += number;
 }
 
 fn consume(buffer: &mut [bool; 20], number: u8, position: &mut u8) {
